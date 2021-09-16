c86e90e1ca975c89ebcab58aa2749f623f41f2d0
Bringfireh <ngutortsenongo@gmail.com>
2021-09-16 09:17:35 -0700

    Enabled migration and edited web.config file

---
+++ QuestersWorld/Migrations/202109161612514_QW.cs
@@ -0,0 +1,16 @@
+namespace QuestersWorld.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class QW : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
